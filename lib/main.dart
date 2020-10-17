29948b5b2c485ccc737b3b0e3746dd467e41db10
Herbert Lin <herbert.lin.7@gmail.com>
2020-10-17 13:12:47 -0500

    add google office markers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
 
 void main() => runApp(MyApp());
 
@@ -9,12 +10,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  GoogleMapController mapController;
+  // GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(32.602798, -85.488960);
+  // final LatLng _center = const LatLng(32.602798, -85.488960);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  // void _onMapCreated(GoogleMapController controller) {
+  //   mapController = controller;
+  // }
+
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
   }
 
   @override
@@ -22,15 +42,18 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
+          title: Text('HackGT7 Sanitizer'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: const LatLng(0, 0),
+            zoom: 2,
+            // target: _center,
+            // zoom: 11.0,
           ),
+          markers: _markers.values.toSet(),
         ),
       ),
     );
