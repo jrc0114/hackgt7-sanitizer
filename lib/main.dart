746787fcfd9f4d3fe871c49c9c48e4afe8281b7f
Herbert Lin <herbert.lin.7@gmail.com>
2020-10-17 19:23:08 -0500

    get user current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'src/locations.dart' as locations;
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,13 +23,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // final Firestore _database = Firestore.instance;
-  final LatLng _center = const LatLng(32.602798, -85.488960);
+  LatLng _center;
+  // LatLng _center = LatLng(32.602798, -85.488960);
   final Map<String, Marker> _markers = {};
 
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    // final googleOffices = await locations.getGoogleOffices();
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _center = LatLng(position.latitude, position.longitude);
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
     setState(() {
       _markers.clear();
       Firestore.instance.collection('restaurant').getDocuments().then((docs) {
@@ -47,17 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         });
       });
-      // for (final office in googleOffices.offices) {
-      //   final marker = Marker(
-      //     markerId: MarkerId(office.name),
-      //     position: LatLng(office.lat, office.lng),
-      //     infoWindow: InfoWindow(
-      //       title: office.name,
-      //       snippet: office.address,
-      //     ),
-      //   );
-      //   _markers[office.name] = marker;
-      // }
     });
   }
 
@@ -66,131 +73,28 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text('HackGT7 Sanitizer')),
       // body: _buildBody(context),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        zoomGesturesEnabled: true,
-        initialCameraPosition: CameraPosition(
-          // target: const LatLng(0, 0),
-          // zoom: 2,
-          target: _center,
-          zoom: 11.0,
-        ),
-        markers: _markers.values.toSet(),
-      ),
+      body: _center == null
+          ? Container(
+              child: Center(
+                child: Text(
+                  'loading map..',
+                  style: TextStyle(
+                      fontFamily: 'Avenir-Medium', color: Colors.grey[400]),
+                ),
+              ),
+            )
+          : GoogleMap(
+              onMapCreated: _onMapCreated,
+              zoomGesturesEnabled: true,
+              initialCameraPosition: CameraPosition(
+                // target: const LatLng(0, 0),
+                // zoom: 2,
+                target: _center,
+                zoom: 15.0,
+              ),
+              myLocationEnabled: true,
+              markers: _markers.values.toSet(),
+            ),
     );
   }
-
-  // Widget _buildBody(BuildContext context) {
-  //   // TODO: get actual snapshot from Cloud Firestore
-  //   return StreamBuilder<QuerySnapshot>(
-  //     stream: Firestore.instance.collection('baby').snapshots(),
-  //     builder: (context, snapshot) {
-  //       if (!snapshot.hasData) return LinearProgressIndicator();
-
-  //       return _buildList(context, snapshot.data.documents);
-  //     },
-  //   );
-  // }
-
-  // Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-  //   return ListView(
-  //     padding: const EdgeInsets.only(top: 20.0),
-  //     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-  //   );
-  // }
-
-  // Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-  //   final record = Record.fromSnapshot(data);
-
-  //   return Padding(
-  //     key: ValueKey(record.name),
-  //     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-  //     child: Container(
-  //       decoration: BoxDecoration(
-  //         border: Border.all(color: Colors.grey),
-  //         borderRadius: BorderRadius.circular(5.0),
-  //       ),
-  //       child: ListTile(
-  //         title: Text(record.name),
-  //         trailing: Text(record.votes.toString()),
-  //         onTap: () =>
-  //             record.reference.updateData({'votes': FieldValue.increment(1)}),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
-
-// class Record {
-//   final String name;
-//   final int votes;
-//   final DocumentReference reference;
-
-//   Record.fromMap(Map<String, dynamic> map, {this.reference})
-//       : assert(map['name'] != null),
-//         assert(map['votes'] != null),
-//         name = map['name'],
-//         votes = map['votes'];
-
-//   Record.fromSnapshot(DocumentSnapshot snapshot)
-//       : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-//   @override
-//   String toString() => "Record<$name:$votes>";
-// }
-
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   // GoogleMapController mapController;
-
-//   // final LatLng _center = const LatLng(32.602798, -85.488960);
-
-//   // void _onMapCreated(GoogleMapController controller) {
-//   //   mapController = controller;
-//   // }
-
-//   final Map<String, Marker> _markers = {};
-//   Future<void> _onMapCreated(GoogleMapController controller) async {
-//     final googleOffices = await locations.getGoogleOffices();
-//     setState(() {
-//       _markers.clear();
-//       for (final office in googleOffices.offices) {
-//         final marker = Marker(
-//           markerId: MarkerId(office.name),
-//           position: LatLng(office.lat, office.lng),
-//           infoWindow: InfoWindow(
-//             title: office.name,
-//             snippet: office.address,
-//           ),
-//         );
-//         _markers[office.name] = marker;
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('HackGT7 Sanitizer'),
-//           backgroundColor: Colors.green[700],
-//         ),
-//         body: GoogleMap(
-//           onMapCreated: _onMapCreated,
-//           initialCameraPosition: CameraPosition(
-//             target: const LatLng(0, 0),
-//             zoom: 2,
-//             // target: _center,
-//             // zoom: 11.0,
-//           ),
-//           markers: _markers.values.toSet(),
-//         ),
-//       ),
-//     );
-//   }
-// }
